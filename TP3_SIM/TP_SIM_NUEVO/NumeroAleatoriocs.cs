714c1d933dc4a3d7311b5bd09275d67cdca6e36a
exe0708 <exe0708@gmail.com>
2021-09-01 20:09:30 -0300

    hola

---
+++ TP3_SIM/TP_SIM_NUEVO/NumeroAleatoriocs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_SIM_NUEVO
+{
+    public class NumeroAleatoriocs
+    {
+        public int Indice { get; set; }
+        public double NroRandom { get; set; }
+
+        public double NroRandom2 { get; set; }
+
+        public double Variable_Aleatoria { get; set; }
+    }
+}
